b7917e8451af1b6e174562ddb7cb0343d0c1fd47
Michael Yockey <myockey@cirrusaircraft.com>
2023-09-05 22:41:28 -0400

    feat: Better tabs and indentation

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,3 +1,10 @@
 vim.opt.colorcolumn = "120"
 vim.opt.undodir = vim.fn.stdpath('data') .. '/undo'
 vim.opt.undofile = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = -1
+vim.opt.shiftwidth = 0
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
